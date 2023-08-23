eb5a43b9d05456739ef6380450557498b7e0b657
Terry Greeniaus <tgreeniaus@phasesensors.com>
2023-08-23 02:39:37 -0700

    working and some constraints

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,50 @@
 import numpy as np
+import itertools
+import math
+
+
+class DiffConstraint:
+    def __init__(self, v):
+        assert 1 <= v <= 8
+        self.perms = []
+        for i in range(1, 10 - v):
+            self.perms.append((i, i + v))
+            self.perms.append((i + v, i))
+
+
+class DivideConstraint:
+    def __init__(self, v):
+        assert 2 <= v <= 9
+        self.perms = []
+        for i in range(1, math.ceil(10 / v)):
+            l, r = i*v, i
+            self.perms.append((l, r))
+            if l != r:
+                self.perms.append((r, l))
+
+
+class MultiplyConstraint:
+    def __init__(self, v, N):
+        perms = set()
+        for i in range(10**(N-1), 10**N):
+            digits = [(i // 10**j) % 10 for j in range(N)]
+            if np.prod(digits) == v:
+                for perm in itertools.permutations(digits):
+                    perms.add(perm)
+        self.perms = list(perms)
+
+
+class AddConstraint:
+    def __init__(self, v, N):
+        perms = set()
+        for i in range(10**(N-1), 10**N):
+            digits = [(i // 10**j) % 10 for j in range(N)]
+            if 0 in digits:
+                continue
+            if np.sum(digits) == v:
+                for perm in itertools.permutations(digits):
+                    perms.add(perm)
+        self.perms = list(perms)
 
 
 def check_equals_ok(board, indice, v):
@@ -32,7 +78,7 @@ def check_divide_ok(board, indices, v):
 
 
 def are_constraints_satisfied(board, r, c):
-    return (check_equals_ok(board, (0, 0), 1) and
+    return (# check_equals_ok(board, (0, 0), 1) and
             check_product_ok(board, [(0, 1), (0, 2)], 40) and
             check_product_ok(board, [(0, 3), (0, 4), (1, 4), (2, 4)], 360) and
             check_sum_ok(board, [(0, 5), (0, 6), (1, 5), (1, 6)], 20) and
@@ -52,13 +98,13 @@ def are_constraints_satisfied(board, r, c):
             check_sum_ok(board, [(4, 5), (5, 5)], 16) and
             check_product_ok(board, [(4, 6), (5, 6), (5, 7)], 90) and
             check_sum_ok(board, [(5, 0), (6, 0), (7, 0)], 16) and
-            check_equals_ok(board, (5, 4), 4) and
+            # check_equals_ok(board, (5, 4), 4) and
             check_sum_ok(board, [(6, 3), (7, 2), (7, 3)], 18) and
             check_product_ok(board, [(6, 4), (7, 4), (8, 4)], 18) and
             check_sum_ok(board, [(6, 5), (7, 5)], 13) and
             check_product_ok(board, [(6, 6), (7, 6)], 36) and
             check_product_ok(board, [(6, 7), (7, 7), (7, 8)], 9) and
-            check_equals_ok(board, (6, 8), 6) and
+            # check_equals_ok(board, (6, 8), 6) and
             check_diff_ok(board, [(8, 0), (8, 1)], 4) and
             check_sum_ok(board, [(8, 2), (8, 3)], 13) and
             check_divide_ok(board, [(8, 5), (8, 6)], 2) and
@@ -68,6 +114,21 @@ def are_constraints_satisfied(board, r, c):
 
 
 def fill_square(board, r, c):
+    # If square was pre-filled, hold it constant.
+    if board[r, c]:
+        if r == 8 and c == 8:
+            return True
+        if c == 8:
+            print()
+            print(board)
+            if fill_square(board, r + 1, 0):
+                return True
+        else:
+            if fill_square(board, r, c + 1):
+                return True
+        return False
+
+    # Otherwise, try all values.
     for v in range(1, 10):
         if v in board[r, :]:
             continue
@@ -89,13 +150,18 @@ def fill_square(board, r, c):
             if fill_square(board, r, c + 1):
                 return True
 
+    # Reset it back to zero.
     board[r, c] = 0
     return False
 
 
-board = np.zeros((9, 9))
-if fill_square(board, 0, 0):
-    print('Found solution:')
-    print(board)
-else:
-    print('No solution.')
+if __name__ == '__main__':
+    board = np.zeros((9, 9))
+    board[0, 0] = 1
+    board[5, 4] = 4
+    board[6, 8] = 6
+    if fill_square(board, 0, 0):
+        print('Found solution:')
+        print(board)
+    else:
+        print('No solution.')
