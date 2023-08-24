ba80dcc48db76f1e223cab7d472b9a73a0663a87
Terry Greeniaus <tgreeniaus@phasesensors.com>
2023-08-23 17:07:50 -0700

    working constraints

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools
 import math
+import sys
 
 
 def diff_perms(v):
@@ -42,7 +43,7 @@ def sum_perms(v, N):
         if np.sum(digits) == v:
             for perm in itertools.permutations(digits):
                 perms.add(perm)
-    perms = list(perms)
+    return list(perms)
 
 
 class Constraint:
@@ -53,33 +54,39 @@ class Constraint:
 
         self.y, self.x = np.transpose(np.array(indices))
         self.perms     = perms
+        self.indices   = indices
 
 
 class Board:
     def __init__(self, H=9, W=9):
-        self.slots         = np.zeros((H, W))
+        self.slots         = np.zeros((H, W), dtype=np.int32)
         self.row_bitmap    = [0] * H
         self.column_bitmap = [0] * W
         self.constraints   = []
+        self.iter          = 0
 
     def __repr__(self):
         return repr(self.slots)
 
     def add_perm(self, p, indices):
+        row_bitmap    = self.row_bitmap[:]
+        column_bitmap = self.column_bitmap[:]
         for v, (r, c) in zip(p, indices):
             assert not self.slots[r, c]
 
             mask = (1 << v)
-            if self.row_bitmap[r] & mask:
+            if row_bitmap[r] & mask:
                 return False
-            if self.column_bitmap[c] & mask:
+            if column_bitmap[c] & mask:
                 return False
 
+            row_bitmap[r]    |= mask
+            column_bitmap[c] |= mask
+
+        self.row_bitmap    = row_bitmap
+        self.column_bitmap = column_bitmap
         for v, (r, c) in zip(p, indices):
-            mask                   = (1 << v)
-            self.slots[r, c]       = v
-            self.row_bitmap[r]    |= mask
-            self.column_bitmap[c] |= mask
+            self.slots[r, c] = v
 
         return True
 
@@ -94,7 +101,7 @@ class Board:
             self.column_bitmap[c] &= ~mask
 
     def add_constant(self, v, index):
-        return self.add_perm((v,), [index])
+        assert self.add_perm((v,), [index])
 
     def add_constraint(self, indices, perms):
         self.constraints.append(Constraint(indices, perms))
@@ -105,7 +112,7 @@ class Board:
     def add_mul_constraint(self, v, indices):
         self.add_constraint(indices, mul_perms(v, len(indices)))
 
-    def add_diff_constraint(self, v, indices):
+    def add_dif_constraint(self, v, indices):
         assert len(indices) == 2
         self.add_constraint(indices, diff_perms(v))
 
@@ -117,10 +124,14 @@ class Board:
         if ci == len(self.constraints):
             return True
 
+        self.iter += 1
+
         c = self.constraints[ci]
         for p in c.perms:
             if self.add_perm(p, c.indices):
                 print(self)
+                # print(' '.join('0x%02X' % b for b in self.row_bitmap))
+                # print(' '.join('0x%02X' % b for b in self.column_bitmap))
                 if self._solve(ci + 1):
                     return True
                 self.del_perm(c.indices)
@@ -131,121 +142,41 @@ class Board:
         return self._solve(0)
 
 
-def check_equals_ok(board, indice, v):
-    return board[indice] == 0 or board[indice] == v
-
-
-def check_sum_ok(board, indices, v):
-    y, x = np.transpose(np.array(indices))
-    a    = board[y, x]
-    s    = np.sum(a)
-    return (not np.all(a) and s < v) or s == v
-
-
-def check_product_ok(board, indices, v):
-    y, x = np.transpose(np.array(indices))
-    a    = board[y, x]
-    p    = np.prod(a)
-    return (not np.all(a) and p <= v) or p == v
-
-
-def check_diff_ok(board, indices, v):
-    y, x = np.transpose(np.array(indices))
-    a    = board[y, x]
-    return not np.all(a) or np.abs(np.diff(a)) == v
-
-
-def check_divide_ok(board, indices, v):
-    y, x = np.transpose(np.array(indices))
-    a    = board[y, x]
-    return not np.all(a) or a[0] / a[1] == v or a[1] / a[0] == v
-
-
-def are_constraints_satisfied(board, r, c):
-    return (# check_equals_ok(board, (0, 0), 1) and
-            check_product_ok(board, [(0, 1), (0, 2)], 40) and
-            check_product_ok(board, [(0, 3), (0, 4), (1, 4), (2, 4)], 360) and
-            check_sum_ok(board, [(0, 5), (0, 6), (1, 5), (1, 6)], 20) and
-            check_sum_ok(board, [(0, 7), (0, 8), (1, 7), (1, 8)], 18) and
-            check_diff_ok(board, [(1, 0), (1, 1)], 3) and
-            check_diff_ok(board, [(1, 2), (2, 2)], 6) and
-            check_divide_ok(board, [(1, 3), (2, 3)], 4) and
-            check_diff_ok(board, [(2, 0), (2, 1)], 3) and
-            check_sum_ok(board, [(2, 5), (3, 3), (3, 4), (3, 5)], 15) and
-            check_divide_ok(board, [(2, 6), (2, 7)], 2) and
-            check_sum_ok(board, [(2, 8), (3, 8), (4, 8), (5, 8)], 20) and
-            check_sum_ok(board, [(3, 0), (3, 1), (4, 0)], 10) and
-            check_product_ok(board, [(3, 2), (4, 2), (5, 2), (5, 3)], 30) and
-            check_sum_ok(board, [(3, 6), (3, 7), (4, 7)], 20) and
-            check_product_ok(board, [(4, 1), (5, 1), (6, 1), (6, 2), (7, 1)], 784) and
-            check_diff_ok(board, [(4, 3), (4, 4)], 2) and
-            check_sum_ok(board, [(4, 5), (5, 5)], 16) and
-            check_product_ok(board, [(4, 6), (5, 6), (5, 7)], 90) and
-            check_sum_ok(board, [(5, 0), (6, 0), (7, 0)], 16) and
-            # check_equals_ok(board, (5, 4), 4) and
-            check_sum_ok(board, [(6, 3), (7, 2), (7, 3)], 18) and
-            check_product_ok(board, [(6, 4), (7, 4), (8, 4)], 18) and
-            check_sum_ok(board, [(6, 5), (7, 5)], 13) and
-            check_product_ok(board, [(6, 6), (7, 6)], 36) and
-            check_product_ok(board, [(6, 7), (7, 7), (7, 8)], 9) and
-            # check_equals_ok(board, (6, 8), 6) and
-            check_diff_ok(board, [(8, 0), (8, 1)], 4) and
-            check_sum_ok(board, [(8, 2), (8, 3)], 13) and
-            check_divide_ok(board, [(8, 5), (8, 6)], 2) and
-            check_diff_ok(board, [(8, 7), (8, 8)], 4) and
-            True
-            )
-
-
-def fill_square(board, r, c):
-    # If square was pre-filled, hold it constant.
-    if board[r, c]:
-        if r == 8 and c == 8:
-            return True
-        if c == 8:
-            print()
-            print(board)
-            if fill_square(board, r + 1, 0):
-                return True
-        else:
-            if fill_square(board, r, c + 1):
-                return True
-        return False
-
-    # Otherwise, try all values.
-    for v in range(1, 10):
-        if v in board[r, :]:
-            continue
-        if v in board[:, c]:
-            continue
-
-        board[r, c] = v
-        if not are_constraints_satisfied(board, r, c):
-            continue
-
-        if r == 8 and c == 8:
-            return True
-        if c == 8:
-            print()
-            print(board)
-            if fill_square(board, r + 1, 0):
-                return True
-        else:
-            if fill_square(board, r, c + 1):
-                return True
-
-    # Reset it back to zero.
-    board[r, c] = 0
-    return False
-
-
 if __name__ == '__main__':
-    board = np.zeros((9, 9))
-    board[0, 0] = 1
-    board[5, 4] = 4
-    board[6, 8] = 6
-    if fill_square(board, 0, 0):
+    b = Board(9, 9)
+    b.add_constant(1, (0, 0))
+    b.add_constant(4, (5, 4))
+    b.add_constant(6, (6, 8))
+    b.add_mul_constraint(40,  [(0, 1), (0, 2)])
+    b.add_mul_constraint(360, [(0, 3), (0, 4), (1, 4), (2, 4)])
+    b.add_sum_constraint(20,  [(0, 5), (0, 6), (1, 5), (1, 6)])
+    b.add_sum_constraint(18,  [(0, 7), (0, 8), (1, 7), (1, 8)])
+    b.add_dif_constraint(3,   [(1, 0), (1, 1)])
+    b.add_dif_constraint(6,   [(1, 2), (2, 2)])
+    b.add_div_constraint(4,   [(1, 3), (2, 3)])
+    b.add_dif_constraint(3,   [(2, 0), (2, 1)])
+    b.add_sum_constraint(15,  [(2, 5), (3, 3), (3, 4), (3, 5)])
+    b.add_div_constraint(2,   [(2, 6), (2, 7)])
+    b.add_sum_constraint(20,  [(2, 8), (3, 8), (4, 8), (5, 8)])
+    b.add_sum_constraint(10,  [(3, 0), (3, 1), (4, 0)])
+    b.add_mul_constraint(30,  [(3, 2), (4, 2), (5, 2), (5, 3)])
+    b.add_sum_constraint(20,  [(3, 6), (3, 7), (4, 7)])
+    b.add_mul_constraint(784, [(4, 1), (5, 1), (6, 1), (6, 2), (7, 1)])
+    b.add_dif_constraint(2,   [(4, 3), (4, 4)])
+    b.add_sum_constraint(16,  [(4, 5), (5, 5)])
+    b.add_mul_constraint(90,  [(4, 6), (5, 6), (5, 7)])
+    b.add_sum_constraint(16,  [(5, 0), (6, 0), (7, 0)])
+    b.add_sum_constraint(18,  [(6, 3), (7, 2), (7, 3)])
+    b.add_mul_constraint(18,  [(6, 4), (7, 4), (8, 4)])
+    b.add_sum_constraint(13,  [(6, 5), (7, 5)])
+    b.add_mul_constraint(36,  [(6, 6), (7, 6)])
+    b.add_mul_constraint(9,   [(6, 7), (7, 7), (7, 8)])
+    b.add_dif_constraint(4,   [(8, 0), (8, 1)])
+    b.add_sum_constraint(13,  [(8, 2), (8, 3)])
+    b.add_div_constraint(2,   [(8, 5), (8, 6)])
+    b.add_dif_constraint(4,   [(8, 7), (8, 8)])
+    if b.solve():
         print('Found solution:')
-        print(board)
+        print(b)
     else:
         print('No solution.')
